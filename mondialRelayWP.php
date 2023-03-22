7e296ebe3b71a304c8c16fabe18a70bc8532e719
saikora11 <pedro.garciaromera@alum.uca.es>
2023-03-22 17:05:32 +0100

    First Commit

---
+++ mondialRelayWP.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @package           mondialRelayWP
+ * @author            Pedro José García Romera
+ * @copyright         2022 Acceso Web
+ * @license           GPL-2.0-or-later
+ * @link              https://github.com/PedroGarcia-BWH/MondialRelayYoreleoWP
+ * @since             alpha-0.0.1
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Modial Relay by Yoreleo
+ * Plugin URI:        https://github.com/PedroGarcia-BWH/MondialRelayYoreleoWP
+ * Description:       Conexión con la API de Mondial Relay para la gestión de envíos y datos de los clientes de Yoreleo
+ * Version:           alpha-0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pedro José García Romera
+ * Author URI:        https://github.com/PedroGarcia-BWH
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       yoreleo-mondial-relay
+ */
+
+
+if (!defined('WPINC')){
+	die;
+}
